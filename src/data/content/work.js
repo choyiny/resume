33f160ad404164ffbfee70feae9008a33f086072
Yong Cho Yin <choyiny@users.noreply.github.com>
2020-06-25 22:29:11 -0400

    IRAP grant resume

--- src/data/content/work.js
+++ src/data/content/work.js
@@ -2,25 +2,18 @@ import React, { Fragment } from 'react';
 
 export const work = [
     {
-        title: 'Full Stack Software Developer',
+        title: 'Full Stack Software Developer (3 Co-op Terms & Contracting)',
         role: 'Verto Inc.',
-        date: 'Apr 2020 - Present',
-        details: [
-            'Reduced time spent by Operations team to deploy by 80% by introducing Infrastructure as Code with Docker and Ansible.',
-            'Led the development of the company\'s core infrastructure with Ruby on Rails, Python, and Redis workers to enable applications to scale up for more users.',
-            'Designed an ETL pipeline with Elasticsearch to allow non-developers to analyse data and generate visualizations on patient health information.',
-            'Entrusted to participate in time critical post-deployment hypercare sessions for major hospitals in the Greater Toronto Area.'
-        ]
-    },
-    {
-        title: 'Full Stack Software Developer (Co-op)',
-        role: 'Verto Inc.',
-        date: 'Sep 2017 - Apr 2020',
+        date: 'Sep 2017 - Present',
         details: [
             'Built an Active Record library on the HL7 FHIR standard with Python, which allowed developers to easily access patient health information programmatically.',
             'Eliminated the need for developers to create customized clinical forms by building a dynamic form renderer and builder with Angular, saving the company over 2 weeks of developer time per implementation.',
             'Created an chat tool built on top of Flask and Kafka which intergrates closely with the company\'s health platform which enhanced clinician\'s communication and work efficiency.',
             'Commended for consistently delivering clean and well-structured code at double the speed of other developers on the team.',
+            'Reduced time spent by Operations team to deploy by 80% by introducing Infrastructure as Code with Docker and Ansible.',
+            'Led the development of the company\'s core infrastructure with Ruby on Rails, Python, and Redis workers to enable applications to scale up for more users.',
+            'Designed an ETL pipeline with Elasticsearch to allow non-developers to analyse data and generate visualizations on patient health information.',
+            'Entrusted to participate in time critical post-deployment hypercare sessions for major hospitals in the Greater Toronto Area.'
         ]
     },
     {
